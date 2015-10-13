ce7a81341e712edd3b527b9a4a6fc1fa8bbb55e0
James Talmage <james@talmage.io>
2015-10-13 19:48:05 -0400

    add defer

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
-module.exports = function (resolve, reject, cb) {
+module.exports = makeResolver;
+module.exports.defer = defer;
+
+function makeResolver(resolve, reject, cb) {
 	return function (err, result) {
 		if (err && reject) {
 			reject(err);
@@ -10,4 +13,18 @@ module.exports = function (resolve, reject, cb) {
 			cb.apply(null, arguments);
 		}
 	};
-};
+}
+
+function defer(cb, Promise) {
+	Promise = Promise || global.Promise;
+	if (typeof Promise !== 'function') {
+		throw new Error('Promise || global.Promise is not a function');
+	}
+	var obj = {};
+	obj.promise = new Promise(function (resolve, reject) {
+		obj.resolve = resolve;
+		obj.reject = reject;
+		obj.cb = makeResolver(resolve, reject, cb);
+	});
+	return obj;
+}
--- test.js
+++ test.js
@@ -1,68 +1,94 @@
 'use strict';
 var sinon = require('sinon');
-var assert = sinon.assert;
+var assertSpy = sinon.assert;
+var assert = require('assert');
 var promiseResolver = require('./');
 
 var resolve;
 var reject;
 var cb;
 var resolver;
+var PromiseStub;
 
 beforeEach(function () {
 	resolve = sinon.spy();
 	reject = sinon.spy();
 	cb = sinon.spy();
+
+	PromiseStub = sinon.spy(function (fn) {
+		fn(resolve, reject);
+		return {
+			then: sinon.spy(),
+			catch: sinon.spy()
+		};
+	});
 });
 
 it('rejects with first argument', function () {
 	resolver = promiseResolver(resolve, reject);
 	var err = new Error('err');
 	resolver(err, 'hello!');
-	assert.notCalled(resolve);
-	assert.calledOnce(reject);
-	assert.calledWith(reject, err);
+	assertSpy.notCalled(resolve);
+	assertSpy.calledOnce(reject);
+	assertSpy.calledWith(reject, err);
 });
 
 it('resolves with second argument', function () {
 	resolver = promiseResolver(resolve, reject);
 	resolver(null, 'howdy!');
-	assert.notCalled(reject);
-	assert.calledOnce(resolve);
-	assert.calledWith(resolve, 'howdy!');
+	assertSpy.notCalled(reject);
+	assertSpy.calledOnce(resolve);
+	assertSpy.calledWith(resolve, 'howdy!');
 });
 
 it('calls callback with error', function () {
 	resolver = promiseResolver(null, null, cb);
 	var err = new Error('err');
 	resolver(err, 'hello!');
-	assert.calledOnce(cb);
-	assert.calledWith(cb, err, 'hello!');
+	assertSpy.calledOnce(cb);
+	assertSpy.calledWith(cb, err, 'hello!');
 });
 
 it('calls callback with result', function () {
 	resolver = promiseResolver(null, null, cb);
 	resolver(null, 'hello!');
-	assert.calledOnce(cb);
-	assert.calledWith(cb, null, 'hello!');
+	assertSpy.calledOnce(cb);
+	assertSpy.calledWith(cb, null, 'hello!');
 });
 
 it('calls reject and callback with error', function () {
 	resolver = promiseResolver(resolve, reject, cb);
 	var err = new Error('err');
 	resolver(err, 'hello!');
-	assert.notCalled(resolve);
-	assert.calledOnce(reject);
-	assert.calledWith(reject, err);
-	assert.calledOnce(cb);
-	assert.calledWith(cb, err, 'hello!');
+	assertSpy.notCalled(resolve);
+	assertSpy.calledOnce(reject);
+	assertSpy.calledWith(reject, err);
+	assertSpy.calledOnce(cb);
+	assertSpy.calledWith(cb, err, 'hello!');
 });
 
 it('calls resolve and callback with result', function () {
 	resolver = promiseResolver(resolve, reject, cb);
 	resolver(null, 'goodbye!');
-	assert.notCalled(reject);
-	assert.calledOnce(resolve);
-	assert.calledWith(resolve, 'goodbye!');
-	assert.calledOnce(cb);
-	assert.calledWith(cb, null, 'goodbye!');
+	assertSpy.notCalled(reject);
+	assertSpy.calledOnce(resolve);
+	assertSpy.calledWith(resolve, 'goodbye!');
+	assertSpy.calledOnce(cb);
+	assertSpy.calledWith(cb, null, 'goodbye!');
+});
+
+it('defer() creates a defer object', function () {
+	var defer = promiseResolver.defer(cb, PromiseStub);
+	resolver = defer.cb;
+	assert.strictEqual(defer.resolve, resolve);
+	assert.strictEqual(defer.reject, reject);
+	assert.strictEqual(typeof defer.promise.then, 'function');
+	assert.strictEqual(typeof defer.promise.catch, 'function');
+
+	resolver(null, 'goodbye!');
+	assertSpy.notCalled(reject);
+	assertSpy.calledOnce(resolve);
+	assertSpy.calledWith(resolve, 'goodbye!');
+	assertSpy.calledOnce(cb);
+	assertSpy.calledWith(cb, null, 'goodbye!');
 });
